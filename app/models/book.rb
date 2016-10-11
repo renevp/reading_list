45a08ef9603cbc36b8dab5f91d5035c2cb3e63dd
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-10-11 21:38:44 +1100

    Model Book modified

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  scope :finished, -> { where.not(finished_on: nil)  }
+  scope :recent, -> { where('finished_on > ?', 2.days.ago) } 
+
   def finished?
     finished_on.present?
   end

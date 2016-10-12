ddc07725e91e0a73a765d5bf78cdbc8fe65b89ab
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-10-12 18:02:52 +1100

    Genres added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012050358) do
+ActiveRecord::Schema.define(version: 20161012055511) do
+
+  create_table "book_genres", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_genres_on_book_id"
+    t.index ["genre_id"], name: "index_book_genres_on_genre_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +31,12 @@ ActiveRecord::Schema.define(version: 20161012050358) do
     t.integer  "rating"
     t.date     "finished_on"
     t.text     "keywords"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

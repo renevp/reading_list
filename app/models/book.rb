29cfcab0457ad21e47703de1ec79f6128b7c4c5e
Rene Vallenilla <rene.vallenilla@gmail.com>
2016-10-12 16:00:52 +1100

    Initial search. Title only.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   scope :finished, -> { where.not(finished_on: nil)  }
-  scope :recent, -> { where('finished_on > ?', 2.days.ago) } 
+  scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+  scope :search, ->(keyword) { where(title: keyword) if keyword.present?  }
 
   def finished?
     finished_on.present?
